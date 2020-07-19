3e05af8adc7dd5a07139c594bda053d4291c55f7
NurdinDev <nureddinbedevi@gmail.com>
2020-07-20 02:06:56 +0300

    :rocket: update README

--- src/request.js
+++ src/request.js
@@ -3,7 +3,7 @@ const API_KEY = "754e66cced9c854e3adffd77926e18a3";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_networks=28`,
   fetchComedyMovie: `/discover/movie?api_key=${API_KEY}&with_networks=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_networks=27`,
